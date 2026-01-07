163f7ab113944d750eea3c9e57706c636e33902f
david <no@email>
2026-01-07 14:25:25 +0100

    fix: import path

--- TODO.md
+++ TODO.md
@@ -5,3 +5,4 @@
 - What is const.LOCALNAMES good for?
 - figure out native vs value and units
 - replace HiLink with Artsauna producer
+- https://hacs.xyz/docs/publish/
--- custom_components/artsauna_ble/artsauna_ble/__init__.py
+++ custom_components/artsauna_ble/artsauna_ble/__init__.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-from artsauna_ble_adapter import ArtsaunaBLEAdapter
-from models import ArtsaunaState
+from .artsauna_ble_adapter import ArtsaunaBLEAdapter
+from .models import ArtsaunaState
 
 __all__ = ["ArtsaunaBLEAdapter", "ArtsaunaState"]
