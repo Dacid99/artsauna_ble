ca15416c65cceb1a5609de9f4c308ac09bba8357
david <no@email>
2026-01-17 14:44:37 +0100

    fix: dont manually flick the switch for heating, the data may be flawd

--- custom_components/artsauna_ble/switch.py
+++ custom_components/artsauna_ble/switch.py
@@ -176,7 +176,8 @@ class ArtsaunaBLESwitch(CoordinatorEntity[ArtsaunaBLECoordinator], SwitchEntity)
                 _LOGGER.error("Wrong KEY for switch: %s", self._key)
                 return
         # manually switch the value for fluid ui
-        self._attr_is_on = not self._attr_is_on
+        if self._key != "heating":  # heating data is flawd
+            self._attr_is_on = not self._attr_is_on
 
     async def async_turn_off(self, **kwargs: Any) -> None:
         await self.async_turn_on()
